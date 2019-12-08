7e9cebe2b33632fcee3abc2837b1b9a39b940074
fyui001 <yun.cod@gmail.com>
2019-12-08 20:33:24 +0900

    eslint修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,55 @@
 // https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint',
-        'ecmaFeatures': {
-            'jsx': true
-        }
-    },
-    env: {
-        browser: true,
-    },
-    extends: [
-        'plugin:react/recommended',
-        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-        'standard'
+  root: true,
+  parserOptions: {
+    parser: [
+      'babel-eslint',
+      '@typescript-eslint/parser'
     ],
-    'plugins': [
-        'react'
-    ],
-    // add your custom rules here
-    rules: {
-        // allow async-await
-        'indent': 'off',
-        'generator-star-spacing': 'off',
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'space-before-function-paren': 'off'
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  env: {
+    browser: true,
+    jest: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'prettier'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  // add your custom rules here
+  rules: {
+
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }
+    ],
+
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'no-undef': 'error',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
